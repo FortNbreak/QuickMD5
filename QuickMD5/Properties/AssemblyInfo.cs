f06f6dc4e04cc532c3a0f039e61740d037c17b78
FortNbreak <98566516+FortNbreak@users.noreply.github.com>
2024-08-23 15:44:06 -0400

    Add files via upload

---
+++ QuickMD5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A fast, free, and open-source MD5 checker by FortNbreak")]
+[assembly: AssemblyDescription("A fast, free, and open-source MD5 checker by FortNbreak")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FortNbreak")]
+[assembly: AssemblyProduct("QuickMD5")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b16b08b8-e216-4c75-a9ed-e181bd9f6c14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
